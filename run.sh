74ee4df26aea0752c74807ab31e80c45bb10cec6
FishHawk <whang177@gmail.com>
2020-04-22 09:23:23 +0800

    Add comment to script

--- run.sh
+++ run.sh
@@ -1,1 +1,8 @@
-node index.js 8080 /home/wh/Projects/DriftingLibrary/test-short-10000/
+# 设置服务器端口号
+port="8080"
+
+# 设置库路径
+library_path="/mnt/DriftingLibrary/library"
+
+# 启动服务器
+node index.js $port $library_path
