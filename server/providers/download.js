17cf7abbf22878ac119445baa74e385f68a78eee
FishHawk <whang177@gmail.com>
2020-06-16 15:57:01 +0800

    Add download manga

--- server/providers/download.js
+++ server/providers/download.js
@@ -17,4 +17,23 @@ async function download_chapter(dir, manga_id, chapter_id) {
   }
 }
 
-export { download_chapter };
+async function download_manga(dir, manga_id) {
+  const detail = await get_detail(manga_id);
+  const mangaDir = path.join(dir, detail.title);
+  if (!fs.existsSync(mangaDir)) fs.mkdirSync(mangaDir);
+
+  for (const [title, collection] of Object.entries(detail.collections)) {
+    const collectionDir = path.join(mangaDir, title);
+    if (!fs.existsSync(collectionDir)) fs.mkdirSync(collectionDir);
+
+    for (const chapter of collection) {
+      const chapterDir = path.join(collectionDir, chapter.title);
+      if (!fs.existsSync(chapterDir)) {
+        fs.mkdirSync(chapterDir);
+        await download_chapter(chapterDir, detail.id, chapter.id);
+      }
+    }
+  }
+}
+
+export { download_chapter, download_manga };
--- test/manhuadui_download.test.js
+++ test/manhuadui_download.test.js
@@ -1,5 +1,8 @@
 import assert from 'assert';
-import { download_chapter } from '../server/providers/download.js';
+import {
+  download_chapter,
+  download_manga,
+} from '../server/providers/download.js';
 
 describe('Provider test: manhuadui', function () {
   // this.skip();
@@ -7,10 +10,22 @@ describe('Provider test: manhuadui', function () {
 
   it('test download chapter', () => {
     // Always pass, need to check manually
-    return download_chapter('/home/wh/Projects/DriftingLibrary/default/', 'guaiguaishouhushen', '261457').then(
-      (result) => {
-        assert.equal(true, true);
-      }
-    );
+    return download_chapter(
+      '/home/wh/Projects/DriftingLibrary/default/',
+      'guaiguaishouhushen',
+      '261457'
+    ).then((result) => {
+      assert.equal(true, true);
+    });
+  });
+
+  it.only('test download manga', () => {
+    // Always pass, need to check manually
+    return download_manga(
+      '/home/wh/Projects/DriftingLibrary/default/',
+      'guaiguaishouhushen'
+    ).then((result) => {
+      assert.equal(true, true);
+    });
   });
 });
