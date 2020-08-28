fec2455d656522b9ffc5471a0940413638a75782
FishHawk <whang177@gmail.com>
2020-08-28 15:11:59 +0800

    Use env

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
-import { port, libraryDir } from './config';
+import fs from 'fs';
+import dotenv from 'dotenv';
+
 import { logger } from './logger';
 import { App } from './app';
+
+dotenv.config();
+
+const port = parseEnvPort(process.env.APP_PORT);
+const libraryDir = parseEnvLibraryDir(process.env.APP_LIBRARY_DIR);
 
 App.createApplication(port, libraryDir)
   .then((app) => {
@@ -9,3 +16,26 @@ App.createApplication(port, libraryDir)
   .catch((e) => {
     logger.error(e);
   });
+
+function parseEnvPort(envPort: string | undefined): number {
+  if (envPort !== undefined) {
+    const port = Number.parseInt(envPort);
+    if (port > 1023 && port <= 65535) {
+      logger.info(`Init: Config port ${port}`);
+      return port;
+    }
+  }
+  logger.error(`Illegal port number: ${envPort}. Should be between 1024 and 65535.`);
+  process.exit(1);
+}
+
+function parseEnvLibraryDir(envDir: string | undefined): string {
+  if (envDir !== undefined) {
+    if (fs.statSync(envDir).isDirectory()) {
+      logger.info(`Init: Config library dir ${envDir}`);
+      return envDir;
+    }
+  }
+  logger.error(`Library dir does not exist.`);
+  process.exit(1);
+}
