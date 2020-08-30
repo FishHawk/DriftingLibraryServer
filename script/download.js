c308d0669c455e3cc7690b08fe6b6578f6a64b75
FishHawk <whang177@gmail.com>
2020-08-30 23:25:30 +0800

    Remove unused script folder

--- script/download.js
+++
@@ -1,6 +0,0 @@
-import download from '../server/provider/download.js';
-import provider from '../server/provider/source/manhuadui.js';
-
-const libraryDir = '/home/wh/Projects/DriftingLibrary/default/';
-const mangaId = 'guaiguaishouhushen';
-download.downloadManga(provider, libraryDir, mangaId);
