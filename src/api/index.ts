e53fee492198033a99054bfe42ca5f6ee3a66e25
FishHawk <whang177@gmail.com>
2021-09-07 22:26:38 +0800

    Fix export error

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,7 +2,6 @@ export { LibraryController } from './controller.library';
 export { ProviderController } from './controller.provider';
 export { SystemController } from './controller.system';
 
-export { cacheControlMiddleware } from './middleware.cache_control';
 export { errorHandleMiddleware } from './middleware.error_handle';
 export { logMiddleware } from './middleware.log';
 
